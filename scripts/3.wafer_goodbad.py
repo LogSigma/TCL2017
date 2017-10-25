6ea09bcf2fae24a6f44cd07e7a988e1872e8f70b
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2017-10-25 22:29:07 +0900

    Update 3.wafer_goodbad.py

--- scripts/3.wafer_goodbad.py
+++ scripts/3.wafer_goodbad.py
@@ -1,1 +1,12 @@
+import os
+import numpy as np
+import pandas as pd
+import scipy as sp
+import skimage as ski
 
+from skimage import filters, transform
+import matplotlib.pyplot as plt
+import itertools as it
+
+good_path_src = "./data/good_img"
+bad_path_src = "./data/bad_img"
