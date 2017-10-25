50379d149cc06eca3f60e7483d3bd0cf63e8ed88
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2017-10-25 22:35:59 +0900

    Update 3.Wafer_GoodBad.py

--- scripts/3.Wafer_GoodBad.py
+++ scripts/3.Wafer_GoodBad.py
@@ -1,20 +1,14 @@
 import os
-import numpy as np
-import pandas as pd
 import scipy as sp
-import skimage as ski
+from glob import glob
 
-from skimage import filters, transform
-import matplotlib.pyplot as plt
-import itertools as it
-
-good_path_src = "./data/good_img"
 bad_path_src = "./data/bad_img"
+good_path_src = "./data/good_img"
 
-bad_list = sorted(glob(dirname + '/' + '*.png'))
+bad_list = sorted(glob(bad_path_src + '/' + '*.png'))
 bad_data = tuple((1, sp.misc.imresize(sp.misc.imread(bad), size=(100, 100))) for bad in bad_list)
 print('Bad Data : %s' % len(bad_data))
 
-good_list = sorted(glob(dirname + '/' + '*.png'))
+good_list = sorted(glob(good_path_src + '/' + '*.png'))
 good_data = tuple((0, sp.misc.imresize(sp.misc.imread(good), size=(100, 100))) for good in good_list)
 print('Good Data : %s' % len(good_data))
