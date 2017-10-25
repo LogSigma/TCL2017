522d2a1c5bba67d40cc7a14b1f4268dc9e34c9a1
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2017-10-25 22:33:15 +0900

    Update 3.wafer_goodbad.py

--- scripts/3.wafer_goodbad.py
+++ scripts/3.wafer_goodbad.py
@@ -10,3 +10,11 @@ import itertools as it
 
 good_path_src = "./data/good_img"
 bad_path_src = "./data/bad_img"
+
+bad_list = sorted(glob(dirname + '/' + '*.png'))
+bad_data = tuple((1, sp.misc.imresize(sp.misc.imread(bad), size=(100, 100))) for bad in bad_list)
+print('Bad Data : %s' % len(bad_data))
+
+good_list = sorted(glob(dirname + '/' + '*.png'))
+good_data = tuple((0, sp.misc.imresize(sp.misc.imread(good), size=(100, 100))) for good in good_list)
+print('Good Data : %s' % len(good_data))
