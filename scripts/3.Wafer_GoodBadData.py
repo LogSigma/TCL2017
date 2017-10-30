62edfa3f9bf1247550dbf61d967fb3d07b5e5fc8
Ji Su Lee <32809393+LogSigma@users.noreply.github.com>
2017-10-30 20:29:44 +0900

    Update and rename 3.Wafer_GoodBad.py to 3.Wafer_GoodBadData.py

---
+++ scripts/3.Wafer_GoodBadData.py
@@ -0,0 +1,27 @@
+import os
+import scipy as sp
+from glob import glob
+
+
+train_bad_path_src = "./data/train/bad_img"
+train_good_path_src = "./data/train/good_img"
+test_bad_path_src = "./data/test/bad_img"
+test_good_path_src = "./data/test/good_img"
+
+
+train_bad_list = sorted(glob(train_bad_path_src + '/' + '*.png'))
+train_bad_data = tuple((1, sp.misc.imresize(sp.misc.imread(bad), size=(100, 100))) for bad in train_bad_list)
+print('TRAIN Bad Data : %s' % len(train_bad_data))
+
+train_good_list = sorted(glob(train_good_path_src + '/' + '*.png'))
+train_good_data = tuple((0, sp.misc.imresize(sp.misc.imread(good), size=(100, 100))) for good in train_good_list)
+print('TRAIN Good Data : %s' % len(train_good_data))
+
+
+test_bad_list = sorted(glob(test_bad_path_src + '/' + '*.png'))
+test_bad_data = tuple((1, sp.misc.imresize(sp.misc.imread(bad), size=(100, 100))) for bad in test_bad_list)
+print('TEST Bad Data : %s' % len(test_bad_data))
+
+test_good_list = sorted(glob(test_good_path_src + '/' + '*.png'))
+test_good_data = tuple((0, sp.misc.imresize(sp.misc.imread(good), size=(100, 100))) for good in test_good_list)
+print('TEST Good Data : %s' % len(test_good_data))
